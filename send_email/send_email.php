a639cd319c7a09dba2c89d647407264c3635dc55
thuandinhdev <97577789+thuandinhdev@users.noreply.github.com>
2024-10-09 15:39:01 +0700

    update

--- send_email/send_email.php
+++ send_email/send_email.php
@@ -22,13 +22,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Port       = 587;
 
         // Cấu hình người gửi và người nhận
-        $mail->setFrom('thuandinh@cuortech.com', 'Your Name');
-        $mail->addAddress($_POST['email']); // Gửi đến email của người dùng
-        $mail->addAddress('thuandinh@cuortech.com'); // Gửi đến admin
+        $mail->setFrom('thuandinh@cuortech.com', 'CUOR');
+        $mail->addAddress($_POST['email']);
 
         // Cấu hình nội dung email
         $mail->isHTML(true);
-        $mail->Subject = 'emai được gửi từ ';
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = 'emai được gửi từ CUOR website';
 
         // Lấy nội dung email từ file
         $email_user_body = file_get_contents('mailbody_user.txt');
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Gửi email cho admin
         $mail->clearAddresses(); // Xóa danh sách người nhận cũ
-        $mail->addAddress('thuandinh@cuortech.com');
+        $mail->addAddress('thuandinh@cuortech.com'); // Gửi đến admin
         $mail->Body    = $email_admin_body;
         $mail->AltBody = strip_tags($email_admin_body);
 
