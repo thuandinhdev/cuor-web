d1b0489ac3d4584c10520c828a98a544f0640daf
thuandinhdev <97577789+thuandinhdev@users.noreply.github.com>
2024-12-13 13:18:52 +0700

    no message

--- js/flexslider.init.js
+++ js/flexslider.init.js
@@ -6,37 +6,31 @@ $(document).ready(function() {
       controlNav: false,   
       animation: "fade",
       start: function(slider) {
-        updateSliderHeight(slider);
+        // updateSliderHeight(slider);
         updateThumbnailActiveClass(slider);
       },
       after: function(slider) {
-        updateSliderHeight(slider);
+        // updateSliderHeight(slider);
         updateThumbnailActiveClass(slider);
       }
     });
-    // $('.thumbnail-link').on('click', function(e) {
     $('.course-feature').on('click', function(e) {
       e.preventDefault(); 
       var index = $(this).data('slide'); 
+      $('.course-feature').removeClass('active'); 
+      $('.course-feature').eq(index).addClass('active'); 
       $('.main-slider').flexslider(index);
     });
   
     function updateThumbnailActiveClass(slider) {
       var currentSlide = slider.currentSlide;
-      // $('.thumbnail-link').removeClass('active'); 
-      // $('.thumbnail-link').eq(currentSlide).addClass('active'); 
       $('.course-feature').removeClass('active'); 
       $('.course-feature').eq(currentSlide).addClass('active'); 
     }
 
-    // Hàm cập nhật chiều cao slider
-    function updateSliderHeight(slider) {
-        // const viewportHeight = $(window).height();
-        // slider.find('.slides').css('height', viewportHeight + 'px');
-        // slider.find('li').css('height', viewportHeight + 'px');
-    }
+    // function updateSliderHeight(slider) {
+    // }
 
-    // Lắng nghe sự kiện resize (nếu màn hình thay đổi)
     $(window).resize(function () {
         updateSliderHeight($('.main-slider'));
     });
